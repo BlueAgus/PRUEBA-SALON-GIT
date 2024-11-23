bcb46a8994e010e2ba93e2d3ef72f7de063beb4f
BlueAgus <micaelamassuco@gmail.com>
2024-11-22 21:50:33 -0300

    agus

---
+++ PRUEBA SALON DE BELLEZA/src/gestores/GestorPrecios.java
@@ -0,0 +1,212 @@
+package gestores;
+
+import enumeraciones.TipoDepilacion;
+import enumeraciones.TipoManicura;
+import enumeraciones.TipoPestanias;
+import excepciones.CodigoNoEncontradoException;
+import model.*;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class GestorPrecios {
+
+    private static final double precioDepiCera = 12000.0;
+    private static final double precioDepiLaser = 18000.0;
+
+    private static final double precioManiGel = 13000.0;
+    private static final double precioManiEsculpidas = 20000.0;
+    private static final double precioManiSemi = 10000.0;
+    private static double precioDisenio = 2000.0;
+
+    private static final double precioPestanias3D = 20000.0;
+    private static final double precioPestanias2D = 16000.0;
+    private static final double precioPestaniasClasic = 14000.0;
+    private static String archivoPrecios = "precios.json";
+    private static Gson gson = new Gson();
+    // mapa anidado
+    private static Map<Class<?>, Map<Enum<?>, Double>> precios = new HashMap<>();
+
+    static {
+        // inicializa los mapas de cada clase vacios
+        precios.put(Depilacion.class, new HashMap<>());
+        precios.put(Manicura.class, new HashMap<>());
+        precios.put(Pestanias.class, new HashMap<>());
+        //clave   vaaaaloooooor
+        // aca le asignamos su otro mapa anidado Mapa(  clase, (enum, precio) );
+        //depilacion                                           clave, valor
+        precios.get(Depilacion.class).put(TipoDepilacion.CERA, precioDepiCera);
+        precios.get(Depilacion.class).put(TipoDepilacion.LASER, precioDepiLaser);
+
+        // manicura
+        precios.get(Manicura.class).put(TipoManicura.GEL, precioManiGel);
+        precios.get(Manicura.class).put(TipoManicura.ESCULPIDAS, precioManiEsculpidas);
+        precios.get(Manicura.class).put(TipoManicura.SEMIPERMANENTE, precioManiSemi);
+
+        //
+        precios.get(Pestanias.class).put(TipoPestanias.TRES_D, precioPestanias3D);
+        precios.get(Pestanias.class).put(TipoPestanias.DOS_D, precioPestanias2D);
+        precios.get(Pestanias.class).put(TipoPestanias.CLASICAS, precioPestaniasClasic);
+    }
+
+
+    public static double obtenerPrecio(Class<?> claseServicio, Enum<?> tipo) {
+        return precios.get(claseServicio).get(tipo);
+        //sacamos primero el valor de la clave "clase" que el mapa anidado, y luego sacamos el valor del segundo mapa que es el precio
+    }
+
+    public static void modificarPrecio(Class<?> claseServicio, Enum<?> tipo, double nuevoPrecio) {
+        Map<Enum<?>, Double> mapaPrecios = precios.get(claseServicio);
+        if (mapaPrecios == null) {
+            throw new IllegalArgumentException("No se encontró un mapa de precios para la clase: " + claseServicio.getName());
+        }
+        if (!mapaPrecios.containsKey(tipo)) {
+            throw new IllegalArgumentException("El tipo " + tipo + " no pertenece a la clase " + claseServicio.getName());
+        }
+        mapaPrecios.put(tipo, nuevoPrecio);
+    }
+
+    // solo va a usarse para  manicura
+    public static double agregarDisenio(Enum<?> tipo) {
+        if (!(tipo instanceof TipoManicura)) {
+            throw new IllegalArgumentException("El diseño solo aplica a servicios de manicura.");
+        }
+        double precioBase = obtenerPrecio(Manicura.class, tipo);
+        return precioBase + precioDisenio;
+    }
+
+    public static double getPrecioDisenio() {
+        return precioDisenio;
+    }
+
+
+    public static void setPrecioDisenio(double nuevoPrecioDisenio) {
+        if (nuevoPrecioDisenio < 0) {
+            throw new ArithmeticException("El precio de diseño no puede ser negativo.");
+        }
+        precioDisenio = nuevoPrecioDisenio;
+    }
+
+    public static void aumentarTodosLosPrecios(double porcentaje) {
+
+        double aumento = 1 + (porcentaje / 100);
+
+        // se usa este en vez de un for porque no es facil de usar con un mapa anidado y no queda lindo
+
+        //               clave,        valor(o sea el mapa anidado)
+        precios.forEach((claseIndice, mapaPreciosIndice) -> {//itera sobre todos los pares clave-valor de precios
+            // y aca iteramos sobre el mapa aniado que contiene el enum y el precio para actualizar
+            mapaPreciosIndice.replaceAll((tipoIndice, precioIndice) -> precioIndice * aumento);
+            //este metodo actualiza los valores del map, toma los valores actuales y modifica
+        });
+    }
+
+    public static void aumentarPreciosPorClase(Class<?> servicio, double porcentaje) {
+        if (porcentaje < 0) {
+            throw new ArithmeticException("El porcentaje de aumento no puede ser negativo.");
+        }
+        double aumento = 1 + (porcentaje / 100);
+
+        // Verificamos si la clase existe en el mapa de precios
+        Map<Enum<?>, Double> mapaPrecios = precios.get(servicio); // obtenemos el valor de precios (la clase anidada)
+        if (mapaPrecios != null) {
+            // el metodo replaceAll reemplaza todos los valores de una
+            mapaPrecios.replaceAll((tipoIndice, precioIndice) -> precioIndice * aumento);
+        } else {
+            throw new IllegalArgumentException("No se encontraron precios para la clase: " + servicio.getSimpleName());
+        }
+    }
+
+    public static void aplicarDescuento(String codigoFactura, double porcentajeDescuento, List<Factura> facturas) throws CodigoNoEncontradoException {
+        // Validar el porcentaje de descuento
+        if (porcentajeDescuento < 0 || porcentajeDescuento > 100) {
+            throw new IllegalArgumentException("El porcentaje de descuento debe estar entre 0 y 100.");
+        }
+        Factura facturaEncontrada = null;
+
+        for (Factura factura : facturas) {
+            if (factura.getCodigoFactura().equals(codigoFactura)) {
+                facturaEncontrada = factura;
+                break; // Salimos del bucle al encontrar la factura
+            }
+        }
+        if (facturaEncontrada == null) {
+            throw new CodigoNoEncontradoException("Factura con código " + codigoFactura + " no encontrada.");
+        }
+
+        double precioOriginal = facturaEncontrada.getPrecioFinal();
+        double descuento = precioOriginal * (porcentajeDescuento / 100);
+
+        // Actualizar el precio final en la factura
+        double nuevoPrecioFinal = precioOriginal - descuento;
+        facturaEncontrada.setPrecioFinal(nuevoPrecioFinal);
+        facturaEncontrada.setDescuento(descuento);
+
+        // Mostrar mensaje informativo (opcional)
+        System.out.println("Descuento del " + porcentajeDescuento + "% aplicado. Descuento: " + descuento + ". Nuevo precio final: " + nuevoPrecioFinal);
+
+        // Retornar el valor del descuento aplicado
+    }
+
+    public static void escribirPreciosEnEnJson() {
+        try (FileWriter writer = new FileWriter(archivoPrecios)) {
+            gson.toJson(precios, writer);
+        } catch (IOException e) {
+            System.out.println("No se puede guardar el archivo de precios: " + e.getMessage());
+        }
+    }
+
+    public static void leerArchivoPrecios() {
+        try (FileReader reader = new FileReader(archivoPrecios)) {
+            Type tipoMapa = new TypeToken<Map<Class<?>, Map<Enum<?>, Double>>>() {
+            }.getType();
+                precios = gson.fromJson(reader, tipoMapa);
+            System.out.println("Archivo de precios leído exitosamente.");
+
+        } catch (IOException e) {
+            System.out.println("No se puede leer el archivo de clientes: " + e.getMessage());
+        }
+    }
+
+    public static String verPrecios() {
+        return
+                "Depilacion con cera: " + precioDepiCera +
+                        "Depilacion con Laser: " + precioDepiLaser +
+                        "Manicura con gel : " + precioManiGel +
+                        "Manicura con esculpidas: " + precioManiEsculpidas +
+                        "Manicura semipermanentes: " + precioManiSemi +
+                        "Precio extra diseño : " + precioDisenio +
+                        "Pestañas 2D: " + precioPestanias2D +
+                        "Pestañas 3d: " + precioPestanias3D +
+                        "Pestañas clasicas: " + precioPestaniasClasic;
+    }
+
+    public static String verPrecioDepi() {
+        return
+                "Depilacion con cera: " + precioDepiCera +
+                        "Depilacion con Laser: " + precioDepiLaser;
+
+    }
+
+    public static String verPrecioPestanias() {
+        return
+                "Pestañas 2D: " + precioPestanias2D +
+                        "Pestañas 3d: " + precioPestanias3D +
+                        "Pestañas clasicas: " + precioPestaniasClasic;
+    }
+
+    public static String verPreciosManicura() {
+        return "Manicura con gel : " + precioManiGel +
+                "Manicura con esculpidas: " + precioManiEsculpidas +
+                "Manicura semipermanentes: " + precioManiSemi +
+                "Precio extra diseño : " + precioDisenio;
+
+    }
+}
---
+++ PRUEBA SALON DE BELLEZA/src/model/Factura.java
@@ -0,0 +1,225 @@
+package model;
+
+import abstractas.Servicio;
+import excepciones.CodigoNoEncontradoException;
+import excepciones.FacturaSinTurnosException;
+import excepciones.TurnoExistenteException;
+import excepciones.TurnoNoExistenteException;
+import enumeraciones.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Factura {
+
+    private String codigoFactura;
+    private TipoDePago tipoPago;
+    private double precioFinal = 0.0;
+    private Cliente cliente;
+    private List<Turno> turnosPorCliente;
+    private double descuento;
+    private double ajuste = 0.0;
+    private String fecha;
+    private String hora;
+    //private GestorServicio gestorServicio;
+
+    //////////////////////////////////////////////////////// CONSTRUCTOR ////////////////////////////////////////////////////
+
+    public Factura(TipoDePago tipoPago, Cliente cliente) {
+
+        this.codigoFactura = this.generarIDEunico(); // aca usamos el metodo de la interfaz directamente
+        this.tipoPago = tipoPago;
+        this.precioFinal = 0.0;
+        this.descuento = 0.0;
+        this.ajuste = 0.0;
+        this.turnosPorCliente = new ArrayList<>();
+        this.cliente = cliente;
+        LocalDate fechaActual = LocalDate.now();
+        LocalTime horaActual = LocalTime.now();
+        this.fecha = convertirFechaAString(fechaActual);
+       // this.hora = convertirFechaAString(horaActual); hacer que este metodo trabaje con horas
+     //   this.gestorServicio = gestorServicio;
+    }
+
+    //////////////////////////////////////////////////////// metodos extr ////////////////////////////////////////////////////
+
+    public static String convertirFechaAString(LocalDate fecha) {
+        DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // Puedes ajustar el formato según necesites
+        return fecha.format(formatoFecha);
+    }
+
+    public String detallesDeServicios() {
+        StringBuilder detalles = new StringBuilder();
+        Map<TipoServicio, Integer> cantidadPorServicio = new HashMap<>();
+
+        for (Turno turno : turnosPorCliente) {
+            try {
+                // Obtener el servicio correspondiente al código
+                Servicio servicio = gestorServicio.buscarServicioCodigo(turno.getCodigo_servicio());
+                TipoServicio tipoServicio = servicio.getTipoService();
+
+                // Incrementar la cantidad de este servicio
+                cantidadPorServicio.put(tipoServicio, cantidadPorServicio.getOrDefault(tipoServicio, 0) + 1);
+            } catch (CodigoNoEncontradoException e) {
+                detalles.append("Servicio no encontrado para el código: ").append(turno.getCodigo_servicio()).append("\n");
+            }
+        }
+
+        // Construir el string de detalles
+        for (Map.Entry<TipoServicio, Integer> entry : cantidadPorServicio.entrySet()) {
+            detalles.append(entry.getKey().name().toLowerCase())
+                    .append(" x")
+                    .append(entry.getValue())
+                    .append("\n");
+        }
+
+        return detalles.toString();
+    }
+
+    public double calcularPrecioFinal() {
+        double precioBase = 0.0;
+
+        for (Turno turno : turnosPorCliente) {
+            try {
+                // Obtener el servicio correspondiente al código
+                Servicio servicio = gestorServicio.buscarServicioCodigo(turno.getCodigo_servicio());
+                precioBase += servicio.calcularPrecio();
+            } catch (CodigoNoEncontradoException e) {
+                System.out.println("Servicio no encontrado para el código: " + turno.getCodigo_servicio());
+            }
+        }
+        this.precioFinal = tipoPago.calcularPagoTotal(precioBase);
+
+        // Calcula explícitamente el ajuste para mostrarlo en la factura
+        this.ajuste = this.precioFinal - precioBase;
+
+        return this.precioFinal;
+    }
+
+    public void agregarTurno(Turno turno) throws TurnoExistenteException {
+        if (turnosPorCliente.contains(turno)) {
+            throw new TurnoExistenteException("El turno ya está ingresado en la factura.");
+        }
+        turnosPorCliente.add(turno);
+    }
+
+    public void eliminarTurno(Turno turno) throws TurnoNoExistenteException, FacturaSinTurnosException {
+        if (!turnosPorCliente.contains(turno)) {
+            throw new TurnoNoExistenteException("El turno que desea eliminar en la factura no existe aqui.");
+        }
+        // opcion 1: no permitimos que se elimine un turno en el caso de que haya solo uno.
+        if (turnosPorCliente.size() == 1) {
+            throw new FacturaSinTurnosException("La factura debe contener al menos un turno, en caso contrario eliminar la factura completa");
+        } //
+
+        turnosPorCliente.remove(turno);
+        System.out.println("El turno fue quitado de la factura final");
+
+        //opcion 2: eliminamos el turno pero dejamos avisado que la instancia de factura no contiene turnos
+       /* if(turnosPorCliente.isEmpty()){
+            throw new FacturaSinTurnosException("La factura no contiene turnos, por favor ingrese un turno para validar la factura");
+        }*/
+    }
+
+    @Override
+    public String generarIDEunico() {
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        return uuid.substring(0, 15);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Factura factura = (Factura) o;
+        return Objects.equals(codigoFactura, factura.codigoFactura);
+    }
+
+    ////////////////////////////////////////////////////////GET Y SET ////////////////////////////////////////////////////
+    public String getCodigoFactura() {
+        return codigoFactura;
+    }
+
+    public TipoDePago getTipoPago() {
+        return tipoPago;
+    }
+
+    public void setTipoPago(TipoDePago tipoPago) {
+        this.tipoPago = tipoPago;
+    }
+
+    public double getPrecioFinal() {
+        return precioFinal;
+    }
+
+    public void setPrecioFinal(double precioFinal) {
+        this.precioFinal = precioFinal;
+    }
+
+    public List<Turno> getTurnosPorCliente() {
+        return turnosPorCliente;
+    }
+
+    public void setTurnosPorCliente(List<Turno> turnosPorCliente) {
+        this.turnosPorCliente = turnosPorCliente;
+    }
+
+    public double getAjuste() {
+        return ajuste;
+    }
+
+    public void setAjuste(double ajuste) {
+        this.ajuste = ajuste;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+
+
+    ////////////////////////////////////// TO STRING ////////////////////////////////////////////////////
+
+    @Override
+    public String toString() {
+        return
+                "| Detalles Factura: " +
+                        "| Metodo de pago: " + tipoPago + "\n" +
+                        "| Precio final : " + precioFinal + "\n" +
+                        "| Descuento aplicado : " + descuento + "\n" +
+                        "| Ajuste aplicado : " + mostrarAjuste() + "\n" +
+                        "| Servicios aplicados : " + detallesDeServicios() + "\n" +
+                        "| Datos del cliente : " + cliente.datosClienteSinGenero() + "\n" +
+                        "| Fecha : " + fecha + "\n" +
+
+                        "=========================================\n";
+    }
+
+    private String mostrarAjuste() {
+        if (ajuste == 0) {
+            return "Sin ajuste";
+        }
+        // aca muestra el signo del numero , si es posi o negativo
+        return String.format("%+.2f", ajuste);
+    }
+}
